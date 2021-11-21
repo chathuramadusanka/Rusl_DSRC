40205a16628af55920ff96d83ff97dc043568b7e
Suthan97 <60068030+Suthan97@users.noreply.github.com>
2021-11-21 13:52:11 +0530

    Add files for people page
    biography.html
    team.html
    assets folder
    cssn folder
    jsn folder
    Forms folder

---
+++ jsn/video.js
@@ -0,0 +1,9 @@
+var player = videojs('my-video_play',{
+    autoplay: 'muted',
+    controls: true,
+    poster: './img/10.jpg',
+    fluid: true,
+    loop: true,
+    playbackRates: [0.75, 1.0, 1.25, 1.5],
+    
+});
